ca1188a8459dc546bfad38235404c4740ab6269f
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2024-07-15 10:54:20 +0900

    Update dis_errno.h

--- dis_errno.h
+++ dis_errno.h
@@ -1,6 +1,9 @@
 #ifndef _DIS_ERRNO_H
 #define _DIS_ERRNO_H
 
+/**
+ * Result of compilation from source code.
+ */
 enum dis_syntax_error {
 	DIS_SYNTAX_OK = 0,
 	DIS_SYNTAX_NON_COMMAND,
@@ -11,6 +14,9 @@ enum dis_syntax_error {
 	DIS_SYNTAX_MAX,
 };
 
+/**
+ * Status of running the Dis program.
+ */
 enum dis_halt_status {
 	DIS_RUNNING = 0,
 	DIS_HALT_COMMAND,
@@ -18,7 +24,14 @@ enum dis_halt_status {
 	DIS_HALT_STATUS_MAX,
 };
 
+/**
+ * Human-readable read-only message
+ */
 const char *get_dis_syntax_error_msg(enum dis_syntax_error);
+
+/**
+ * Human-readable read-only message
+ */
 const char *get_dis_halt_status_msg(enum dis_halt_status);
 
 #endif /* _DIS_ERRNO_H */
