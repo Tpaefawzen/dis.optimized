4ebbc6bcbc53cc31f28e1c5197fdb99db2c42f07
Tpaefawzen <100011199+Tpaefawzen@users.noreply.github.com>
2024-07-15 11:43:11 +0900

    Described dis_math.h

--- dis_math.h
+++ dis_math.h
@@ -6,13 +6,32 @@
 
 #include "dis.h"
 
+/**
+ * Maximum integer represented in given base and digits.
+ */
 dis_addr_t DIS_INT_MAX(const dis_base_t base, const dis_digits_t digits);
+
+/**
+ * DIS_INT_MAX() + 1; pow(base, digits)
+ */
 dis_addr_t DIS_INT_END(const dis_base_t base, const dis_digits_t digits);
 
+/**
+ * Move lowest digit to top while shifting other digits to right in specified base.
+ */
 dis_int_t rotate(const dis_base_t, const dis_digits_t, const dis_int_t);
+
+/**
+ * 1. Represent x and y with given base.
+ * 2. For each digit in x and y, subtract one from another as given base without borrow.
+ * 3. Return the result.
+ */
 dis_int_t subtract_without_borrow(const dis_base_t, dis_digits_t,
 		dis_int_t, dis_int_t);
 
+/**
+ * Addition in human math rule. Wrap around.
+ */
 dis_addr_t dis_addr_add(
 		const dis_base_t base, const dis_digits_t digits,
 		const dis_addr_t x, const dis_addr_t y);
